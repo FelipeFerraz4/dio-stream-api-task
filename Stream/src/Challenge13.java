4cbf5cc03edefc0269ff826705ccc5bb8c2eb5cd
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 15:45:29 -0300

    create answer challenge 13

--- Stream/src/Challenge13.java
+++ Stream/src/Challenge13.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Challenge13 {
     public static void main(String[] args) {
@@ -10,5 +11,12 @@ public class Challenge13 {
          */
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        
+        List<Integer> numbreBetween5And10 = numbers.stream()
+            .filter(number -> number >= 5)
+            .filter(number -> number <= 10)
+            .collect(Collectors.toList());
+
+        System.out.println(numbreBetween5And10);
     }
 }
