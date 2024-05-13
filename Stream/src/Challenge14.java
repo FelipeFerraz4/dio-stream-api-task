2bfdf2a110ec8f5a54899fa3deecb2eebfb2f470
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 16:32:31 -0300

    create answer challenge 14

--- Stream/src/Challenge14.java
+++ Stream/src/Challenge14.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Challenge14 {
     public static void main(String[] args) {
@@ -11,6 +12,33 @@ public class Challenge14 {
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
+        //1º answer
+        // Optional<Integer> largestPrimeNumber = numbers.stream()
+        //     .filter(number -> isPrimeNumber(number))
+        //     .sorted((a, b) -> b.compareTo(a))
+        //     .findFirst();
         
+        // System.out.println(largestPrimeNumber.get());
+
+        //2º answer
+        Optional<Integer> largestPrimeNumber = numbers.stream()
+            .filter(number -> isPrimeNumber(number))
+            .max((numb1, numb2) -> Integer.compare(numb1, numb2));
+
+        System.out.println(largestPrimeNumber.get());
+        
+    }
+
+    private static boolean isPrimeNumber(int number) {
+        if (number == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
