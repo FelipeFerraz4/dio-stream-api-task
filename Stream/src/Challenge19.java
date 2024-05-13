8b5797e0c31d8eaaa9920a034d926008c620239f
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 16:08:10 -0300

    create answer challenge 19

--- Stream/src/Challenge19.java
+++ Stream/src/Challenge19.java
@@ -10,5 +10,21 @@ public class Challenge19 {
          */
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        //1º answer
+        // Integer sumOfDivisorsFor3And5 = numbers.stream()
+        //     .filter(number -> number % 3 == 0)
+        //     .filter(number -> number % 5 == 0)
+        //     .reduce(0, (sum, number) -> sum + number);
+
+        // System.out.println(sumOfDivisorsFor3And5);
+
+        //2º answer
+        Integer sumOfDivisorsFor3And5 = numbers.stream()
+            .filter(number -> number % 3 == 0)
+            .filter(number -> number % 5 == 0)
+            .mapToInt(number -> number.intValue())
+            .sum();
+        System.out.println(sumOfDivisorsFor3And5);
     }
 }
