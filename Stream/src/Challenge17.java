4811fed435c24f0ec9e14a54cef847e8637fc2d3
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 16:26:10 -0300

    create answer challenge 17

--- Stream/src/Challenge17.java
+++ Stream/src/Challenge17.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Challenge17 {
     public static void main(String[] args) {
@@ -10,5 +11,24 @@ public class Challenge17 {
          */
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> primeNumbers = numbers.stream()
+            .filter(number -> isPrimeNumber(number))
+            .collect(Collectors.toList());
+        
+        System.out.println(primeNumbers);
+    }
+
+    private static boolean isPrimeNumber(int number) {
+        if (number == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
