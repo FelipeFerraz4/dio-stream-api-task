0e0e2bb323f7308a05bcc839c807ed85fc1150e6
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 15:41:36 -0300

    create answer challenge 12

--- Stream/src/Challenge12.java
+++ Stream/src/Challenge12.java
@@ -10,5 +10,8 @@ public class Challenge12 {
          */
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Integer productNumber = numbers.stream().reduce(1, (product, number) -> product*number);
+        System.out.println(productNumber);
     }
 }
