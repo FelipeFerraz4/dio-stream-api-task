e1f94eaa552d4cb0ceff74d5a6dbea24297e8a81
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 16:37:31 -0300

    create answer challenge 18

--- Stream/src/Challenge18.java
+++ Stream/src/Challenge18.java
@@ -10,5 +10,10 @@ public class Challenge18 {
          */
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean isEqualNumbers = numbers.stream()
+            .allMatch(number -> number.equals(numbers.get(0)));
+        
+        System.out.println(isEqualNumbers);
     }
 }
