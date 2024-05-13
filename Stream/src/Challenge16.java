ffeee877f4356f5e917b8d09a9090c7d2693aa8d
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 15:56:50 -0300

    create answer challenge 16

--- Stream/src/Challenge16.java
+++ Stream/src/Challenge16.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Challenge16 {
     public static void main(String[] args) {
@@ -12,5 +14,15 @@ public class Challenge16 {
          */
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Map<Boolean, List<Integer>> numbersEvenOdd = numbers.stream()
+            .collect(Collectors.partitioningBy(number -> number % 2 == 0));
+        
+        List<Integer> even = numbersEvenOdd.get(true);
+        List<Integer> odd = numbersEvenOdd.get(false);
+
+        System.out.println(numbersEvenOdd);
+        System.out.println(even);
+        System.out.println(odd);
     }
 }
