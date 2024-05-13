3062149cb109d74309e99ed0fad56ddf6dc93ffd
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 15:13:24 -0300

    create answer challenge 9

--- Stream/src/Challenge9.java
+++ Stream/src/Challenge9.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Challenge9 {
     public static void main(String[] args) {
@@ -11,5 +12,8 @@ public class Challenge9 {
          */
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean diferentNumber = numbers.size() == numbers.stream().collect(Collectors.toSet()).size();
+        System.out.println(diferentNumber);
     }
 }
