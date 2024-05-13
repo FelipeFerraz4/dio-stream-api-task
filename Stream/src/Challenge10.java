a74bcc32f63ff4459d7f9a79fd9cf5a545a8d93d
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-13 15:39:17 -0300

    create answer challenge 10

--- Stream/src/Challenge10.java
+++ Stream/src/Challenge10.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Challenge10 {
     public static void main(String[] args) {
@@ -10,5 +12,12 @@ public class Challenge10 {
          */
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Map<Boolean, List<Integer>> numberOddMultiple3or5 = numbers
+            .stream()
+            .filter(number -> (number % 2 == 1))
+            .filter(number -> number % 3 == 0 || number % 5 == 0)
+            .collect(Collectors.partitioningBy(number -> number % 3 == 0));
+        System.out.println( numberOddMultiple3or5);
     }
 }
