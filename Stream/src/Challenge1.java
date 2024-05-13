0d8e6d19eb5a0b1876f7b77034c38a50e89d788b
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-12 23:51:27 -0300

    creating answer for challenge 1 and 2

---
+++ Stream/src/Challenge1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Challenge1 {
+    public static void main(String[] args) throws Exception {
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        /*
+         * Desafio 1 - Mostre a lista na ordem numérica:
+         * Crie um programa que utilize a Stream API para ordenar 
+         * a lista de números em ordem crescente e a exiba no console.
+         */
+        
+        //1º response
+        // Collections.sort(numbers);
+        // System.out.println(numbers);
+
+        //2º response
+        List<Integer> orderedNumbers = numbers.stream().sorted().collect(Collectors.toList());
+        System.out.println(orderedNumbers);
+    }
+}
---
+++ Stream/src/Challenge2.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Challenge2 {
+    public static void main(String[] args) {
+        /*
+        * Desafio 2 - Imprima a soma dos números pares da lista:
+        * Utilizando a Stream API, realize a soma dos números pares da lista
+        * e exiba o resultado no console.
+        */
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        //1º response
+        // int sumTotal = 0;
+        // int sumEven = 0;
+        // for (Integer number : numbers) {
+        //     sumTotal += number;
+        //     if (number % 2 == 0) {
+        //         sumEven += number;
+        //     }
+        // }
+
+        // System.out.println(sumTotal);
+        // System.out.println(sumEven);
+
+        //2º response
+        int sum = numbers.stream().mapToInt(number -> number.intValue()).sum();
+        
+        int sumEven = numbers.stream().filter(number -> number % 2 == 0).mapToInt(number -> number.intValue()).sum(); 
+
+        System.out.println(sum);
+        System.out.println(sumEven);
+    }
+}
